7573394aab54fee67171901dc06ed532830af3af
hanyongle <hanyongle@zgzb.com>
2023-11-21 16:19:32 +0800

    init

---
+++ src/views/about/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="console">
+        3333333333333333333
+    </div>
+  </template>
+  <script lang="ts" setup>
+    
+  </script>
+  
+  <style lang="less" scoped></style>
+  
---
+++ src/views/dashboard/workplace/workplace.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="console">
+      22222222222222222222222222222222
+  </div>
+</template>
+<script lang="ts" setup>
+  
+</script>
+
+<style lang="less" scoped></style>
